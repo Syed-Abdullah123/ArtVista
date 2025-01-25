f48f9a68dfedd04d8c297996a73bd92f0f182d6f
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2025-01-25 12:35:27 +0500

    Contains the firebase configurations, auth, firestore, storage

--- ArtVista/firebaseConfig.ts
+++ ArtVista/firebaseConfig.ts
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzaRRDm9g8suRS3kk7MpXXwk5vCV_PGeQ",
-  authDomain: "artvista-firebase.firebaseapp.com",
-  projectId: "artvista-firebase",
-  storageBucket: "artvista-firebase.firebasestorage.app",
-  messagingSenderId: "829413563633",
-  appId: "1:829413563633:web:f0dae925bcee5592731d50",
+  apiKey: "AIzaSyD5C-nsvOUs8C4BvcpHcPBNShfTWnCz3yM",
+  authDomain: "fir-fyp-bf23a.firebaseapp.com",
+  projectId: "fir-fyp-bf23a",
+  storageBucket: "fir-fyp-bf23a.appspot.com",
+  messagingSenderId: "274371607874",
+  appId: "1:274371607874:web:c013030e809f7210c49cdb",
 };
 
 const FIREBASE_APP = initializeApp(firebaseConfig);
@@ -17,3 +18,4 @@ export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
